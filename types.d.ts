d41e606c5f2a9128764b923bc7bff793d623656b
doniahusien <doniahusien@gmail.com>
2025-10-28 17:02:01 +0300

    update types for response

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,35 @@
 import type { AxiosInstance } from 'axios'
 
+interface Country {
+  id: number
+  name: string
+  phone_code: string
+  phone_limit: number
+  flag: string
+}
+
+interface User {
+  id: number
+  full_name: string
+  avatar: string
+  email: string
+  phone_code: string
+  phone: string
+  user_type: 'customer' | 'admin' | string
+  notifiable: boolean
+  token: string
+  points: number
+  wallet: number
+  country: Country
+  default_address: unknown | null
+  tenant: string
+}
+
+interface AuthStore {
+  token: string | null
+  userData: User | null
+}
+
 declare module '#app' {
   interface NuxtApp {
     $api: AxiosInstance
@@ -12,4 +42,8 @@ declare module 'vue' {
   }
 }
 
-export {}
+interface ApiResponse<T> {
+  data: T
+  status: string
+  message: string
+}
