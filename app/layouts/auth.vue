01162e01ed31be9568f0aebce45162a99e7bbdba
doniahusien <doniahusien@gmail.com>
2025-10-30 00:36:14 +0300

    update layout of auth

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,33 +1,34 @@
 <template>
-   <Html :lang="locale" :dir="locale == 'ar' ? 'rtl' : 'ltr'">
-    <Body :dir="locale == 'ar' ? 'rtl' : 'ltr'">
-  <div class="flex min-h-[100vh]">
-    <div class="auth_bg w-[650px] 3xl:w-[800px] hidden xl:flex flex-col relative">
-      <img
-        src="/images/bg/auth_bg.png"
-        alt="Auth background"
-        class="m-auto min-w-[130px]"
-      />
+  <Html :lang="locale" :dir="locale === 'ar' ? 'rtl' : 'ltr'">
+    <Body :dir="locale === 'ar' ? 'rtl' : 'ltr'">
+      <div class="min-h-screen flex items-stretch gap-5">
+        <div class="container auth_bg hidden xl:flex flex-col relative overflow-hidden">
+          <img
+            src="/images/bg/auth_bg.png"
+            alt="Auth background"
+            class="object-cover w-full h-full"
+          />
 
-      <NuxtLink :to="switchPath" class="absolute top-6 right-6 bg-white flex items-center gap-2 text-text w-10 h-10 rounded-full justify-center">
-        {{ t(`locale.${locale}`) }}
-      </NuxtLink>
-    </div>
-
-    <div
-      class="max-w-[600px] px-10 p-4 w-full auth_wrapper flex flex-col mx-auto space-y-9 overflow-y-auto"
-    >
-      <slot />
-    </div>
-  </div>
+          <NuxtLink
+            :to="switchPath"
+            class="absolute top-6 right-6 bg-white text-gray-800 flex items-center justify-center w-10 h-10 rounded-full shadow hover:bg-gray-100 transition"
+          >
+            {{ t(`locale.${locale}`) }}
+          </NuxtLink>
+        </div>
+        <div class="container mx-auto flex items-center justify-center px-4 lg:px-20 py-10">
+          <div class="w-full">
+            <slot />
+          </div>
+        </div>
+      </div>
     </Body>
   </Html>
-
 </template>
 
 <script setup>
-const { locale,t, setLocale } = useI18n();
+const { locale, t } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
-const targetLocale = computed(() => locale.value === 'ar' ? 'en' : 'ar')
+const targetLocale = computed(() => (locale.value === 'ar' ? 'en' : 'ar'))
 const switchPath = computed(() => switchLocalePath(targetLocale.value))
 </script>
