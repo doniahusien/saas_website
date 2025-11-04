72fc3ced2b16ff22cfa331fffc0bad6f0522dc2e
doniahusien <doniahusien@gmail.com>
2025-11-04 13:01:03 +0200

    update filter style

--- app/components/UI/FilterSection.vue
+++ app/components/UI/FilterSection.vue
@@ -1,92 +1,137 @@
 <template>
-  <aside class="w-full bg-white rounded-2xl p-5 shadow-md">
-    <div class="flex items-center justify-between mb-4">
-      <h2 class="font-bold text-lg">Selected filters</h2>
-      <button @click="clearAll" class="text-red-500 text-sm hover:underline">Clear all</button>
-    </div>
-    <UInput
-      v-model="search"
-      icon="i-heroicons-magnifying-glass-20-solid"
-      placeholder="Search"
-      class="mb-4"
-    />
-
-    <div v-if="selectedFilters.length" class="flex flex-wrap gap-2 mb-6">
-      <UBadge
-        v-for="(filter, index) in selectedFilters"
-        :key="index"
-        color="primary"
-        variant="solid"
-        class="flex items-center gap-1"
-      >
-        {{ filter }}
-        <button @click="removeFilter(filter)" class="ml-1 text-xs">✕</button>
-      </UBadge>
-    </div>
-
-    <div class="mb-6">
-      <h3 class="text-gray-400 text-sm font-medium mb-2">Select main Categories</h3>
-      <div class="flex flex-wrap gap-2">
-        <UButton
-          v-for="cat in mainCategories"
-          :key="cat"
-          :variant="selectedMain === cat ? 'solid' : 'outline'"
-          color="primary"
-          class="rounded-xl text-sm px-4 py-2"
-          @click="selectMain(cat)"
-        >
-          {{ cat }}
-        </UButton>
-      </div>
-    </div>
+  <aside
+    class="w-full bg-white rounded-2xl p-5 shadow-md flex flex-col justify-between h-full"
+  >
     <div>
-      <h3 class="text-gray-400 text-sm font-medium mb-2">Select sub category</h3>
-      <div class="grid grid-cols-2 gap-2">
-        <UButton
-          v-for="sub in subCategories"
-          :key="sub"
-          :variant="selectedSub === sub ? 'solid' : 'outline'"
-          color="primary"
-          class="rounded-xl text-sm px-4 py-2"
-          @click="selectSub(sub)"
-        >
-          {{ sub }}
-        </UButton>
+      <div class="flex items-center justify-between mb-4">
+        <h2 class="font-bold text-lg">Selected filters</h2>
+        <button @click="clearAll" class="text-red-500 text-sm hover:underline">
+          Clear all
+        </button>
       </div>
+
+      <UInput
+        v-model="search"
+        icon="i-heroicons-magnifying-glass-20-solid"
+        placeholder="Search"
+        :ui="{
+          base: ' w-full border text-black border-light-gray bg-white',
+          wrapper: 'rounded-xl',
+          icon: 'text-gray-400',
+          input: 'text-sm  placeholder:text-gray-400',
+        }"
+      />
+      <div v-if="selectedFilters.length" class="flex flex-wrap gap-2 my-6">
+        <UBadge
+          v-for="(filter, index) in selectedFilters"
+          :key="index"
+          variant="solid"
+          :ui="{
+            base:
+              'bg-light-gray text-btn flex items-center gap-1 rounded-full text-xs font-medium px-2 py-1',
+          }"
+        >
+          {{ filter }}
+          <button @click="removeFilter(filter)" class="ml-1 text-xs cursor-pointer">
+            <UIcon name="i-lucide-x" class="w-2 h-2" />
+          </button>
+        </UBadge>
+      </div>
+
+      <div class="my-10 space-y-4">
+        <h3 class="text-gray-400 text-sm font-medium">Select main category</h3>
+        <div class="grid grid-cols-2 gap-2">
+          <UButton
+            v-for="cat in mainCategories"
+            :key="cat"
+            :variant="selectedMain === cat ? 'solid' : 'outline'"
+            :ui="{
+              base:
+                'flex items-center justify-center cursor-pointer rounded-xl text-sm font-medium transition-all duration-200',
+              rounded: 'rounded-full',
+              padding: 'px-0 py-0',
+            }"
+            :class="selectedMain === cat ? 'bg-btn text-white' : ' bg-white'"
+            @click="selectMain(cat)"
+          >
+            {{ cat }}
+          </UButton>
+        </div>
+      </div>
+
+      <div class="space-y-4">
+        <h3 class="text-gray-400 text-sm font-medium">Select sub category</h3>
+        <div class="grid grid-cols-2 gap-2">
+          <UButton
+            v-for="sub in subCategories"
+            :key="sub"
+            :variant="selectedSub === sub ? 'solid' : 'outline'"
+           
+            :ui="{
+              base:'flex items-center justify-center cursor-pointer rounded-xl text-sm font-medium transition-all duration-200',
+               rounded: 'rounded-full',
+              padding: 'px-0 py-0',
+        
+            }"
+            :class="selectedSub === Sub ? 'bg-btn text-white' : ' bg-white'"
+            @click="selectSub(sub)"
+          >
+            {{ sub }}
+          </UButton>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-6">
+      <button
+        class= 'w-full py-3 bg-btn text-white rounded-full font-semibold text-sm',
+      >
+        Apply Filters
+      </button>
     </div>
   </aside>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref } from "vue";
 
-const search = ref('')
-const selectedMain = ref('')
-const selectedSub = ref('')
-const selectedFilters = ref<string[]>([])
+const search = ref("");
+const selectedMain = ref("");
+const selectedSub = ref("");
+const selectedFilters = ref<string[]>([]);
 
-const mainCategories = ['Food', 'Drink', 'Dessert']
-const subCategories = ['Burger', 'Pizza', 'Chicken', 'Salad', 'Grill']
+const mainCategories = ["Food", "Drink", "Dessert"];
+const subCategories = ["Burger", "Pizza", "Chicken", "Salad", "Grill"];
 
 const selectMain = (cat: string) => {
-  selectedMain.value = cat
-  if (!selectedFilters.value.includes(cat)) selectedFilters.value.push(cat)
-}
+  selectedMain.value = cat;
+  if (!selectedFilters.value.includes(cat)) selectedFilters.value.push(cat);
+};
 
 const selectSub = (sub: string) => {
-  selectedSub.value = sub
-  if (!selectedFilters.value.includes(sub)) selectedFilters.value.push(sub)
-}
+  selectedSub.value = sub;
+  if (!selectedFilters.value.includes(sub)) selectedFilters.value.push(sub);
+};
 
 const removeFilter = (filter: string) => {
-  selectedFilters.value = selectedFilters.value.filter(f => f !== filter)
-  if (selectedMain.value === filter) selectedMain.value = ''
-  if (selectedSub.value === filter) selectedSub.value = ''
-}
+  selectedFilters.value = selectedFilters.value.filter((f) => f !== filter);
+  if (selectedMain.value === filter) selectedMain.value = "";
+  if (selectedSub.value === filter) selectedSub.value = "";
+};
 
 const clearAll = () => {
-  selectedMain.value = ''
-  selectedSub.value = ''
-  selectedFilters.value = []
-}
+  selectedMain.value = "";
+  selectedSub.value = "";
+  selectedFilters.value = [];
+  search.value = "";
+};
+
+const applyFilters = () => {
+  console.log("Applied filters:", {
+    search: search.value,
+    main: selectedMain.value,
+    sub: selectedSub.value,
+    filters: selectedFilters.value,
+  });
+};
 </script>
