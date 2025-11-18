8b2f3915d15d263b5ad186c113a30f1fa42ae7a1
doniahusien <doniahusien@gmail.com>
2025-11-18 13:33:53 +0200

    update success adding

--- app/components/Modal/AddAddresModal.vue
+++ app/components/Modal/AddAddresModal.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-semi-white rounded-2xl flex flex-col overflow-hidden shadow-lg p-6 relative w-full md:w-1/3 h-auto mx-4"
+    class="bg-semi-white rounded-2xl flex flex-col overflow-hidden  p-6 relative w-full md:w-1/3 h-auto mx-4"
   >
     <button class="absolute top-8 right-5 text-black">
       <Icon name="mdi:close" class="cursor-pointer w-7 h-7" />
@@ -57,11 +57,50 @@
 
       <div class="mt-4">
         <button
+          @click="saveAddress"
           class="w-full cursor-pointer bg-btn text-white gap-2 py-3 rounded-full font-semibold flex items-center justify-center"
         >
           Save
         </button>
       </div>
     </div>
+    <transition name="fade-up">
+      <div
+        v-if="showSuccess"
+        class="absolute inset-0 z-40 flex items-end bg-black/60 backdrop-blur-xs rounded-2xl"
+      >
+        <div
+          class="w-full flex flex-col justify-end bg-white rounded-2xl h-50 shadow-xl p-6 text-center z-50"
+        >
+          <h2 class="text-2xl font-bold">Con-Grates</h2>
+          <p class="text-gray-500 text-sm mt-1">Your Address Has Been Added Successfully</p>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const showSuccess = ref(false);
+
+const saveAddress = () => {
+  showSuccess.value = true;
+  setTimeout(() => {
+    showSuccess.value = false;
+  }, 6000);
+};
+</script>
+
+<style scoped>
+.fade-up-enter-active,
+.fade-up-leave-active {
+  transition: all 0.35s ease;
+}
+.fade-up-enter-from,
+.fade-up-leave-to {
+  opacity: 0;
+  transform: translateY(10px);
+}
+</style>
