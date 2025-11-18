013df0cb10934a9ee79a4b1a60f0de1ab3f69436
doniahusien <doniahusien@gmail.com>
2025-11-18 13:11:17 +0200

    update modal add address

--- app/components/Modal/AddAddresModal.vue
+++ app/components/Modal/AddAddresModal.vue
@@ -1,13 +1,61 @@
 <template>
   <div
-    class="bg-light-white rounded-2xl flex flex-col overflow-hidden shadow-lg p-6 relative w-full md:w-1/3 h-120 mx-4"
+    class="bg-semi-white rounded-2xl flex flex-col overflow-hidden shadow-lg p-6 relative w-full md:w-1/3 h-auto mx-4"
   >
-    <button class="absolute top-4 right-4 text-gray-600 hover:text-black">
-      <Icon name="mdi:close" class="cursor-pointer w-6 h-6" />
+    <button class="absolute top-8 right-5 text-black">
+      <Icon name="mdi:close" class="cursor-pointer w-7 h-7" />
     </button>
-    <h2 class="text-2xl font-bold mb-4">Add New Address</h2>
-    <div class="space-y-4 bg-white">
-      <div class="mt-auto">
+
+    <h2 class="text-3xl font-bold mb-6">Add new address</h2>
+
+    <div class="space-y-4">
+      <div>
+        <label class="block text-lg font-semibold mb-1">Title</label>
+        <input
+          type="text"
+          placeholder="Home2"
+          class="w-full bg-white text-placeholder focus:outline-0 rounded-xl p-5 text-sm"
+        />
+      </div>
+
+      <div>
+        <label class="block text-lg font-semibold mb-1">Address</label>
+        <input
+          type="text"
+          placeholder="Enter Your Address Here"
+          class="w-full bg-white text-placeholder focus:outline-0 rounded-xl p-5 text-sm"
+        />
+      </div>
+
+      <div>
+        <label class="block text-lg font-semibold mb-1">Building No.</label>
+        <input
+          type="text"
+          placeholder="Enter Your Building No. Here"
+          class="w-full bg-white text-placeholder focus:outline-0 rounded-xl p-5 text-sm"
+        />
+      </div>
+      <div class="flex items-center gap-4">
+        <div class="flex-1">
+          <label class="block text-lg font-semibold mb-1">Floor No.</label>
+          <input
+            type="text"
+            placeholder="Enter Your Floor No. Here"
+            class="w-full bg-white text-placeholder focus:outline-0 rounded-xl p-5 text-sm"
+          />
+        </div>
+
+        <div class="flex-1">
+          <label class="block text-lg font-semibold mb-1">Apartment No.</label>
+          <input
+            type="text"
+            placeholder="Enter Your Apartment No. Here"
+            class="w-full bg-white text-placeholder focus:outline-0 rounded-xl p-5 text-sm"
+          />
+        </div>
+      </div>
+
+      <div class="mt-4">
         <button
           class="w-full cursor-pointer bg-btn text-white gap-2 py-3 rounded-full font-semibold flex items-center justify-center"
         >
