2cc89c3ee335a232ff74cb955ac4cabfc5e221cd
doniahusien <doniahusien@gmail.com>
2025-11-24 16:47:37 +0200

    fix

--- app/plugins/axios.ts
+++ app/plugins/axios.ts
@@ -10,9 +10,10 @@ export default defineNuxtPlugin((nuxtApp) => {
   const appStore = useAppStore()
   const branchCookie = useCookie<Branch | null>('selectedBranch')
   const storeId = computed(() => branchCookie.value?.id || null)
-  const sessionToken = computed(() => appAuth.isLoggedIn)
+ /*  const sessionToken = computed(() => appAuth.isLoggedIn) */
 
   const router = useRouter()
+  const sessionToken = useCookie<string | null>('jwt_token_saas')
   const guest_token = useCookie('user_guest_token')
   const localePath = useLocalePath()
 
@@ -27,63 +28,48 @@ export default defineNuxtPlugin((nuxtApp) => {
     },
   })
 
-  api.interceptors.request.use(
-    (req: AxiosRequestConfig & { headers?: any; params?: any } = {}) => {
-      req.headers = req.headers || {}
+ api.interceptors.request.use(
+  (req: AxiosRequestConfig & { headers?: any; params?: any; skipLocation?: boolean } = {}) => {
+    req.headers = req.headers || {}
+    req.params = req.params || {}
 
-      req.headers['Accept-Language'] = i18n?.locale?.value || 'en'
+    req.headers['Accept-Language'] = i18n?.locale?.value || 'en'
 
-      if (sessionToken.value) {
-        req.headers.Authorization = `Bearer ${sessionToken.value}`
-      } else if (guest_token?.value) {
-        req.params = {
-          ...req.params,
-          guest_token: guest_token.value,
-        }
-      }
-
-
-      req.params = { ...(req.params || {}) }
-
-/*        if (storeId.value) {
-        req.params.store_id = req.params.store_id ?? storeId.value
-      } */
-      const userDataLocal = appAuth.getUserData || appAuth.userData
-      const rawUserLocation = useCookie('userLocation').value
-
-      let userLocation: { lat?: number; lng?: number } | null = null
-      if (rawUserLocation) {
-        if (typeof rawUserLocation === 'string') {
-          try {
-            userLocation = JSON.parse(rawUserLocation)
-          } catch (e) {
-            userLocation = null
-          }
-        } else {
-          userLocation = rawUserLocation
-        }
-      }
-
-      if (userDataLocal) {
-        const defaultAddressId = userDataLocal?.default_address?.id
-        if (defaultAddressId) {
-          req.params.address_id = req.params.address_id ?? defaultAddressId
-        } else if (userLocation?.lat != null && userLocation?.lng != null) {
-          req.params.lat = req.params.lat ?? userLocation.lat
-          req.params.lng = req.params.lng ?? userLocation.lng
-        }
-      } else if (userLocation?.lat != null && userLocation?.lng != null) {
-        
-        req.params.lat = req.params.lat ?? userLocation.lat 
-        req.params.lng = req.params.lng ?? userLocation.lng
-      }
-
-      return req
-    },
-    (error) => {
-      return Promise.reject(error)
+    if (sessionToken.value) {
+      req.headers.Authorization = `Bearer ${sessionToken.value}`
+    } else if (guest_token?.value) {
+      req.params.guest_token = req.params.guest_token ?? guest_token.value
     }
-  )
+
+    if (req.skipLocation) {
+      return req
+    }
+    const userDataLocal = appAuth.getUserData || appAuth.userData
+    const rawUserLocation = useCookie('userLocation').value
+
+    let userLocation: { lat?: number; lng?: number } | null = null
+    if (rawUserLocation) {
+      try {
+        userLocation =
+          typeof rawUserLocation === 'string'
+            ? JSON.parse(rawUserLocation)
+            : rawUserLocation
+      } catch {
+        userLocation = null
+      }
+    }
+
+    if (userDataLocal?.default_address?.id) {
+      req.params.address_id = req.params.address_id ?? userDataLocal.default_address.id
+    } else if (userLocation?.lat && userLocation?.lng) {
+      req.params.lat = req.params.lat ?? userLocation.lat
+      req.params.lng = req.params.lng ?? userLocation.lng
+    }
+
+    return req
+  }
+)
+
 
   api.interceptors.response.use(
     (res) => res,
