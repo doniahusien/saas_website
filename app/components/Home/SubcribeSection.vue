ad0f0471c6933435e447ab5104666e4e7884eae6
doniahusien <doniahusien@gmail.com>
2025-12-08 10:14:01 +0200

    fix email input

--- app/components/Home/SubcribeSection.vue
+++ app/components/Home/SubcribeSection.vue
@@ -20,31 +20,38 @@
           class="text-base md:text-lg text-text-ligh"
         ></div>
 
-        <VeeForm
-          @submit="handleSubmit"
-          :validation-schema="schema"
-          class="grid grid-cols-1 md:grid-cols-3 gap-4 rounded-full bg-website-white"
-        >
-          <div class="col-span-2">
-            <VeeField
-              name="email"
-              type="email"
-              :placeholder="$t('subscription.placeholder')"
-              class="rounded-full border border-line bg-website-white text-text focus:outline-none px-3 py-6 w-full"
-            />
-            <VeeErrorMessage
-              name="email"
-              class="text-red-500 text-xs mt-1 block text-start"
-            />
-          </div>
+<VeeForm
+  @submit="handleSubmit"
+  :validation-schema="schema"
+  v-slot="{ meta }"
+  class="grid grid-cols-1 md:grid-cols-3 gap-4 rounded-full bg-website-white"
+>
+  <div class="col-span-2">
+    <VeeField
+      name="email"
+      type="email"
+      :placeholder="$t('subscription.placeholder')"
+      class="rounded-full border border-line bg-website-white text-text focus:outline-none px-3 py-6 w-full"
+    />
+    <VeeErrorMessage
+      name="email"
+      class="text-red-500 text-xs mt-1 block text-start"
+    />
+  </div>
 
-          <button
-            type="submit"
-            class="w-40 cursor-pointer rounded-full p-4 font-medium text-website-white bg-primary hover:opacity-90 transition ms-auto"
-          >
-            {{ $t("subscription.cta") }}
-          </button>
-        </VeeForm>
+  <button
+    type="submit"
+    :disabled="!meta.valid"
+    class="w-40 rounded-full p-4 font-medium text-website-white bg-primary transition ms-auto"
+    :class="{
+      'opacity-40 cursor-not-allowed': !meta.valid,
+      'hover:opacity-90 cursor-pointer': meta.valid
+    }"
+  >
+    {{ $t("subscription.cta") }}
+  </button>
+</VeeForm>
+
       </div>
     </div>
   </section>
@@ -61,9 +68,10 @@ defineProps({
 });
 
 const schema = object({
-  email: string().email(`${t("errors.emailRequired")}`),
+  email: string()
+    .required(t("errors.emailRequired"))
+    .email(t("errors.emailRequired")),
 });
-
 async function handleSubmit(values) {
   try {
     const { data } = await $api.post("/news-letter-subscription", {
